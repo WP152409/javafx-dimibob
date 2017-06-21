aaca9811715109c472817ca89efa068670722499
ChalkPE <chalk@chalk.pe>
2017-06-21 10:51:07 +0900

    Add error handler and progress indicator

--- src/pe/chalk/meal/Controller.java
+++ src/pe/chalk/meal/Controller.java
@@ -1,10 +1,16 @@
 package pe.chalk.meal;
 
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.layout.StackPane;
 
 import java.lang.reflect.Field;
 import java.time.Instant;
@@ -14,9 +20,11 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Stream;
 
 public class Controller {
+    @FXML private StackPane stack;
     @FXML private DatePicker calendar;
 
     @FXML @SuppressWarnings("unused") private ListView<String> breakfast;
@@ -24,22 +32,39 @@ public class Controller {
     @FXML @SuppressWarnings("unused") private ListView<String> dinner;
     @FXML @SuppressWarnings("unused") private ListView<String> snack;
 
+    private ProgressIndicator progress;
+
     @FXML public void initialize() {
+        progress = new ProgressIndicator();
+        StackPane.setMargin(progress, new Insets(50));
+
         calendar.setValue(LocalDate.now());
         calendar.setConverter(new DateConverter());
+
+        search(null);
     }
 
     public void search(@SuppressWarnings("unused") final ActionEvent event) {
-        try {
-            final Instant instant = calendar.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant();
-            final Map<String, List<String>> meal = Fetcher.getMeal(Date.from(instant));
+        stack.getChildren().add(progress);
 
-            Stream.of(getClass().getDeclaredFields())
-                    .filter(field -> field.getType().equals(ListView.class))
-                    .forEach(field -> updateList(meal, field));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        new Thread(() -> {
+            try {
+                final LocalDate date = calendar.getValue();
+                if (Objects.isNull(date)) return;
+
+                final Instant instant = date.atStartOfDay(ZoneId.systemDefault()).toInstant();
+                final Map<String, List<String>> meal = Fetcher.getMeal(Date.from(instant));
+
+                Platform.runLater(() -> Stream.of(getClass().getDeclaredFields())
+                        .filter(field -> field.getType().equals(ListView.class))
+                        .forEach(field -> updateList(meal, field)));
+            } catch (Exception e) {
+                e.printStackTrace();
+                Platform.runLater(() -> new Alert(Alert.AlertType.ERROR, e.getMessage(), ButtonType.CLOSE).show());
+            } finally {
+                Platform.runLater(() -> stack.getChildren().remove(progress));
+            }
+        }).start();
     }
 
     @SuppressWarnings("unchecked")
--- src/pe/chalk/meal/Main.java
+++ src/pe/chalk/meal/Main.java
@@ -10,12 +10,12 @@ public class Main extends Application {
     @Override
     public void start(final Stage stage) throws Exception {
         final Parent root = FXMLLoader.load(getClass().getResource("meal.fxml"));
+        stage.setScene(new Scene(root, 400, 700));
 
         stage.setTitle("디미밥");
-        stage.setScene(new Scene(root, 400, 700));
-        stage.show();
+        stage.setOnCloseRequest(value -> System.exit(0));
 
-        root.requestFocus();
+        stage.show();
     }
 
     public static void main(String[] args) {
